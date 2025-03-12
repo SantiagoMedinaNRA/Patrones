18123914dcff43d763f7878a12a172e25c618043
SantiagoMedinaNRA <santiago.medina@numrot.com>
2025-03-12 08:08:07 -0500

    feat(punto1): add hero classes and UML diagram
    Updated the solution to include a new project "Punto1" with a class hierarchy for `Heroe`, `Guerrero`, `Mago`, and `Arquero`. Implemented cloning and customization features in `Program.cs`. Updated project settings to target .NET 8.0. Added a UML diagram and PNG representation for visual reference. Defined the `Heroe` abstract class with properties and methods for hero management.

---
+++ Punto1/Personaje/Arquero.cs
@@ -0,0 +1,24 @@
+﻿namespace Punto1.Personaje
+{
+  internal class Arquero : Heroe
+  {
+    public Arquero()
+    {
+      Clase = "Arquero";
+      Habilidades.Add("Disparo Preciso");
+      Habilidades.Add("Evasion");
+      Habilidades.Add("Triple Tiro");
+      Apariencia.Add("Armadura de cuero");
+      Apariencia.Add("Capucha");
+      Apariencia.Add("Botas de cuero");
+    }
+
+    public override Heroe Clone()
+    {
+      var copia = (Arquero)MemberwiseClone();
+      copia.Habilidades = [.. Habilidades];
+      copia.Apariencia = [.. Apariencia];
+      return copia;
+    }
+  }
+}
